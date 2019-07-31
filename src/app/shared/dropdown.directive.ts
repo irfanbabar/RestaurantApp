eaf5409bbe90629d16a3a8e1296c7313619851ff
irfanbabar <irfanbabar99@gmail.com>
2019-08-01 01:05:59 +0500

    modified dropdown directive code, now dropdown close if click anywhere, also resolved design issues of navbar.

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,15 +1,23 @@
-import { Directive, Input, HostBinding, HostListener, OnInit } from "@angular/core";
+import { Directive, Input, HostBinding, HostListener, OnInit, ElementRef } from "@angular/core";
 
 @Directive({
   selector: '[ngDropdown]'
 })
 
 export class DropdownDirective implements OnInit {
+  // this will toggle open class when click dropdown
+  // @HostBinding('class.open') isOpen: boolean;
+  // @HostListener('click') toggleClass() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  // this will open dropdown on clicking it and close it if we click to anywhere in document
   @HostBinding('class.open') isOpen: boolean;
-  @HostListener('click') toggleClass() {
-    this.isOpen = !this.isOpen;
+  @HostListener('document:click', ['$event']) toggleClass() {
+    console.log(event.target)
+    this.isOpen = this.elemRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
-  constructor() {}
+  constructor(private elemRef: ElementRef) {}
 
   ngOnInit() {
     this.isOpen = false;
